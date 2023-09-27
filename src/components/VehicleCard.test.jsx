a2b908957f4144c37ef22a0cb46189748e412699
Lukas <lukasdrinkwater97@gmail.com>
2023-09-27 21:56:32 +0100

    continued working on VehicleCard tests

--- src/components/VehicleCard.test.jsx
+++ src/components/VehicleCard.test.jsx
@@ -39,45 +39,24 @@ describe("Vehicle card tests", () => {
       type: "car",
     };
     render(<VehicleCard car={car} />);
-    // const addButton = screen.getByText(/-/);
     const countFieldPlaceholder = screen.getByPlaceholderText("0");
     const countField = screen.getByRole("textbox");
-    // expect(countField.value).toBe("0");
+  });
+
+  it("check if input shows current count of vehicle", () => {
+    const car = {
+      id: 1,
+      make: "make",
+      model: "model",
+      year: 1998,
+      price: 25,
+      disc: "discription",
+      img: "img",
+      count: 1,
+      type: "car",
+    };
+    render(<VehicleCard car={car} />);
+    const countField = screen.getByRole("textbox");
+    expect(countField.value).toBe("1");
   });
 });
-
-it("check if input shows current count of vehicle", () => {
-  const car = {
-    id: 1,
-    make: "make",
-    model: "model",
-    year: 1998,
-    price: 25,
-    disc: "discription",
-    img: "img",
-    count: 1,
-    type: "car",
-  };
-  render(<VehicleCard car={car} />);
-  const countField = screen.getByRole("textbox");
-  expect(countField.value).toBe("1");
-});
-
-// describe("App component", () => {
-//   it("renders magnificent monkeys", () => {
-//     // since screen does not have the container property, we'll destructure render to obtain a container for this test
-//     const { container } = render(<App />);
-//     expect(container).toMatchSnapshot();
-//   });
-
-//   it("renders radical rhinos after button click", async () => {
-//     const user = userEvent.setup();
-
-//     render(<App />);
-//     const button = screen.getByRole("button", { name: "Click Me" });
-
-//     await user.click(button);
-
-//     expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
-//   });
-// });
