5e8b6103b2e3aa6a8acf21ab09556f32c1477c5a
Lukas <lukasdrinkwater97@gmail.com>
2023-09-28 19:17:14 +0100

    added filter to rendering CarCards.

--- src/pages/cars/CarsContainerSetup.jsx
+++ src/pages/cars/CarsContainerSetup.jsx
@@ -10,13 +10,16 @@ const CarsContainerSetup = ({ carData, handleCountChange }) => {
       <h1>Here are the cars for sale </h1>
       <div className="gridContainer">
         {/* something to make each car card */}
-        {carData.map((car) => (
-          <VehicleCard
-            car={car}
-            key={car.id}
-            handleCountChange={handleCountChange}
-          />
-        ))}
+        {carData.map(
+          (car) =>
+            car.type === "car" && (
+              <VehicleCard
+                car={car}
+                key={car.id}
+                handleCountChange={handleCountChange}
+              />
+            )
+        )}
       </div>
     </>
   );
